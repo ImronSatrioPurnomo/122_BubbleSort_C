798e84dd209ace94586ababf7cca689aab72d4e7
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-03-08 10:50:47 +0700

    menambahkan Input

--- SortingData/SortingData.cpp
+++ SortingData/SortingData.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array: ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai makasimal 20 elemen.\n";
+		}
+	}
+}
+
